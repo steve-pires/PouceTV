dffff2109ceb6e303f96876e850b42c8af619da3
Baztoune <baztoune@gmail.com>
2011-05-07 13:54:57 +0200

    Reset DB à chaque démarrage

--- app/jobs/Bootstrap.java
+++ app/jobs/Bootstrap.java
@@ -9,11 +9,9 @@ import play.test.Fixtures;
 @OnApplicationStart
 public class Bootstrap extends Job {
 	public void doJob() {
-		if (User.count() == 0) {
-			Fixtures.deleteAllModels();
-			Fixtures.loadModels("../test/users.yml");
-			Fixtures.loadModels("../test/data.yml");
-			Logger.info("RESET DB");
-		}
+        Fixtures.deleteAllModels();
+        Fixtures.loadModels("../test/users.yml");
+        Fixtures.loadModels("../test/data.yml");
+        Logger.info("RESET DB");
 	}
 }
